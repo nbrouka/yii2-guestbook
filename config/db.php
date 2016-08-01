64506d347436ff6925431b1d2e4e21146bce3c49
nbrouka <nadzeya.brouka@gmail.com>
2016-08-01 07:13:15 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=guestbook',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=php-yii2guestbook.rhcloud.com;dbname=php',
+    'username' => 'adminWR2my3k',
+    'password' => ' LiFh8xpT8Btf',
     'charset' => 'utf8',
 ];
